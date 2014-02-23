6699de2a9c4e7446de1dd9bd5898b769682e971a
Oliver Schwarz <oliver.schwarz@gmail.com>
2014-02-23 20:14:36 +0000

    Typo and structure changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,32 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-      sass: {
-        dist: {
-          files: {
-            'css/style.css' : 'scss/style.scss'
-          }
-        }
-      },
-      watch: {
-        css: {
-          files: '**/*.scss',
-          tasks: ['sass']
-          }
+    sass: {
+      dist: {
+        files: {
+          'css/style.css' : 'scss/style.scss'
         }
       }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+        }
+      },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+    }
   );
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.registerTask('default',['watch']);
 }
