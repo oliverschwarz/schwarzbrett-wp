cc9f317fe6f6f4781355f19d8130cdc8325270f3
Oliver Schwarz <oliver.schwarz@gmail.com>
2014-02-23 20:13:25 +0000

    Minor changes

--- header.php
+++ header.php
@@ -13,4 +13,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php wp_title( '|', true, 'right' ); ?><?php echo bloginfo( 'name' ); ?></title>
 <?php wp_head(); ?>
+
+<link rel="stylesheet" href="css/style.css">
+  
 </head>
